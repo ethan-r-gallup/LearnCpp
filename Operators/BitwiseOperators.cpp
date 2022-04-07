7cad4bdb128b05be267f928c676689e957b0370c
ethan-r-gallup <ethanrobertgallup@gmail.com>
2022-04-06 22:04:02 -0600

    Update BitwiseOperators.cpp

---
+++ Operators/BitwiseOperators.cpp
@@ -0,0 +1,11 @@
+/************************************************************************
+ * BITWISE OPERATORS
+ * every bitwise operator is performed on each bit of the number rather 
+ * than a value.
+ * &   performs AND on every bit of two numbers
+ * |   performs OR on every bit of two numbers
+ * ^   performs XOR on every bit of two numbers
+ * <<  shifts the bits of the 1st operand LEFT the by the num of the 2nd
+ * >>  shifts the bits of the 1st operand RIGHT the by the num of the 2nd
+ * ~   inverts all the bits of a single operand
+ * **********************************************************************/
